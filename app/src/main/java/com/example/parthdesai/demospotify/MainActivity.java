9bb49b0766772be12a9bef5a8ddc37531d712630
Parth Desai <parth1493@gmail.com>
2017-09-20 11:14:11 -0400

    Integrate spotify with app

--- app/src/main/java/com/example/parthdesai/demospotify/MainActivity.java
+++ app/src/main/java/com/example/parthdesai/demospotify/MainActivity.java
@@ -3,11 +3,54 @@ package com.example.parthdesai.demospotify;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
 
+public class MainActivity extends AppCompatActivity implements
+       PlayerNotificationCallback, ConnectionStateCallback {
+    // TODO: Replace with your client ID
+    private static final String CLIENT_ID = "4aa6997f64684e70a5f4576eae90e80f";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "6f54108baaf643c4885ca0dcc25ccc54";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onLoggedIn() {
+
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String s) {
+
     }
 }
