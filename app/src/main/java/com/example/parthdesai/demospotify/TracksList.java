f62e059a6a8790e393071336a6dd3a28a554e534
Parth Desai <parth1493@gmail.com>
2017-09-24 21:50:09 -0400

    Track adaptor and class added

--- app/src/main/java/com/example/parthdesai/demospotify/TracksList.java
+++ app/src/main/java/com/example/parthdesai/demospotify/TracksList.java
@@ -9,12 +9,15 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.parthdesai.demospotify.Adaptor.ItemListAdaptor;
 import com.example.parthdesai.demospotify.model.Items;
@@ -26,38 +29,42 @@ import com.squareup.moshi.Moshi;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class PlayList extends AppCompatActivity {
-
-    private String TAG = PlayList.class.getSimpleName();
+public class TracksList extends AppCompatActivity {
+    String userID;
+    String playListName;
+    String playListId;
+    TextView playListNameTextView;
+    private String TAG = TracksList.class.getSimpleName();
     private ListView listView;
     private ItemListAdaptor userPlayListAdaptor;
     private ProgressDialog pDialog;
     private BroadcastReceiver receiver;
-    private ArrayList<Items> playListItemList = null;
+//    private ArrayList<Items> playListItemList = null;
     private int resumeFlag = 0;
     String httpString =null;
-    String userID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_list);
+        setContentView(R.layout.activity_tracks_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        userID = getIntent().getStringExtra("USERID");
-        httpString = "https://api.spotify.com/v1/users/"+userID+"/playlists";
         initXmlView();
+        userID = getIntent().getStringExtra("USERID");
+        playListName = getIntent().getStringExtra("PLAYLISTNAME");
+        playListId = getIntent().getStringExtra("PLAYLISTID");
+        playListNameTextView.setText(playListName);
+        httpString = "https://api.spotify.com/v1/users/"+userID+"/playlists/"+playListId+"/tracks";
     }
-
     private void initXmlView() {
-        listView = (ListView) findViewById(R.id.play_list);
-        playListItemList = new ArrayList<>();
+        playListNameTextView = (TextView)findViewById(R.id.list_name);
+        listView = (ListView) findViewById(R.id.track_list);
+       // playListItemList = new ArrayList<>();
     }
     @Override
     protected void onResume() {
         resumeFlag = 0;
         super.onResume();
         registerReceiver();
-
     }
     public void registerReceiver() {
         Log.d("Check internet","User partiicpant register");
@@ -77,14 +84,13 @@ public class PlayList extends AppCompatActivity {
         if(ValidationClass.checkOnline(getApplicationContext())){
             if(resumeFlag == 0) {
                 resumeFlag = 1;
-                 new GetData().execute();
+                new GetData().execute();
             }
         }else{
             resumeFlag = 0;
         }
 
     }
-
     /**
      * Async task class to get json by making HTTP call
      */
@@ -94,7 +100,7 @@ public class PlayList extends AppCompatActivity {
         protected void onPreExecute() {
             super.onPreExecute();
             // Showing progress dialog
-            pDialog = new ProgressDialog(PlayList.this);
+            pDialog = new ProgressDialog(TracksList.this);
             pDialog.setMessage("Please wait...");
             pDialog.setCancelable(false);
             pDialog.show();
@@ -112,19 +118,19 @@ public class PlayList extends AppCompatActivity {
                 Log.d(TAG, response );
 
                 Moshi moshi = new Moshi.Builder().build();
-                JsonAdapter<PlayListItem> playlistItemAdapter = moshi.adapter(PlayListItem.class);
-                PlayListItem playlistitem = null;
-                try {
-                    playlistitem = playlistItemAdapter.fromJson(response);
+//                JsonAdapter<PlayListItem> playlistItemAdapter = moshi.adapter(PlayListItem.class);
+//                PlayListItem playlistitem = null;
+//                try {
+//                    playlistitem = playlistItemAdapter.fromJson(response);
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+               // Log.d(TAG, "doInBackground() called with: " + "params = [" + playlistItemAdapter.toJson(playlistitem) + "]");
 
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Log.d(TAG, "doInBackground() called with: " + "params = [" + playlistItemAdapter.toJson(playlistitem) + "]");
-
-                for(Items p:playlistitem.getItems()){
-                    playListItemList.add(p);
-                }
+//                for(Items p:playlistitem.getItems()){
+//                   / playListItemList.add(p);
+//                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -145,19 +151,19 @@ public class PlayList extends AppCompatActivity {
         }
     }
     public void setViewList(){
-        userPlayListAdaptor = new ItemListAdaptor(getApplicationContext(),playListItemList);
-        listView.setAdapter(userPlayListAdaptor);
-        userPlayListAdaptor.notifyDataSetChanged();
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d(TAG,playListItemList.get(i).getName());
-                Intent intent = new Intent(PlayList.this,TracksList.class);
-                intent.putExtra("USERID",userID);
-                intent.putExtra("PLAYLISTNAME",playListItemList.get(i).getName());
-                intent.putExtra("PLAYLISTID",playListItemList.get(i).getId());
-                startActivity(intent);
-            }
-        });
+//        userPlayListAdaptor = new ItemListAdaptor(getApplicationContext(),playListItemList);
+//        listView.setAdapter(userPlayListAdaptor);
+//        userPlayListAdaptor.notifyDataSetChanged();
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Log.d(TAG,playListItemList.get(i).getName());
+//                Intent intent = new Intent(PlayList.this,TracksList.class);
+//                intent.putExtra("USERID",userID);
+//                intent.putExtra("PLAYLISTNAME",playListItemList.get(i).getName());
+//                intent.putExtra("PLAYLISTID",playListItemList.get(i).getId());
+//                startActivity(intent);
+//            }
+//        });
     }
 }
